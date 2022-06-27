d493587886dc698a1d12f72d2a4f0e228da3d7fd
Britishexchangestudent <44755600+Britishexchangestudent@users.noreply.github.com>
2022-06-27 12:42:20 +0100

    deploying first

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+const { getRedirectStatus } = require("next/dist/lib/load-custom-routes");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/canceled",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
